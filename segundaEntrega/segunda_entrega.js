7ba121c0de4683dde8d37e2eb8fa25ba42755215
gabriel yucra <gabriel.yucra_99@hotmail.com>
2022-07-27 22:10:54 -0300

    second commit

---
+++ segundaEntrega/segunda_entrega.js
@@ -0,0 +1,23 @@
+
+function validarNota() {
+    if(nota>0 && nota<=10){
+        acum = acum + nota;
+        cont = cont+1;
+    }
+    else{
+        alert(`Ingrese un numero entre el 1 y el 10`);
+    }
+}
+
+
+
+let acum=0, cont=0, prom;
+
+for(let i=0; i<3; i++){
+    nota= parseInt(prompt(`Ingrese la nota del trimestre ${i+1}`));
+    validarNota()
+}
+prom=acum/cont;
+if(cont==3){
+    alert(`El promedio anual es ${prom}`)
+}
