00c65fb03b2862c0551bec40b0330d98194a8149
gabriel yucra <gabriel.yucra_99@hotmail.com>
2022-08-12 22:44:10 -0300

    segunda entrega del proyecto final

--- 7_SegundaEntrega/segunda Entrega.js
+++ 7_SegundaEntrega/segunda Entrega.js
@@ -6,17 +6,43 @@ class Cliente{
     this.testimonio = testimonio;
     }
 }
+function crearObj(nombre, apellido, edad, testimonio){
+    const cliente = new Cliente(nombre, apellido, edad, testimonio)
+    array.push(cliente)
+    localStorage.setItem('cliente', JSON.stringify(array))
+}
+
+const dark = document.getElementById('m-dark')
+const claro = document.getElementById('m-claro')
+
+let darkMode;
+
+if(localStorage.getItem("darkMode")) {
+    darkMode = localStorage.getItem("darkMode")
+} 
+    else {
+        localStorage.setItem("darkMode", "claro")
+    }
+
+if(darkMode == "dark") {
+    document.body.classList.add("modo-oscuro")
+}
+
+dark.addEventListener('click', () => {
+    document.body.classList.add("modo-oscuro")
+    localStorage.setItem('darkMode', "dark")
+})
+
+claro.addEventListener('click', () => {
+    document.body.classList.remove("modo-oscuro")
+    localStorage.setItem('darkMode', "light")
+})
 
 const formulario = document.getElementById("form")
 const array = [];
 
-function crearObj(nombre, apellido, edad, testimonio){
-    const cliente = new Cliente(nombre, apellido, edad, testimonio)
-    array.push(cliente)
-}
-
 formulario.addEventListener(`submit`, (e)=>{
-    e.preventDefault()
+    e.preventDefault()   
 
     let nombre = document.getElementById('name').value
     let apellido = document.getElementById('lastName').value
@@ -24,6 +50,7 @@ formulario.addEventListener(`submit`, (e)=>{
     let testimonio = document.getElementById('experiencia').value
 
     crearObj(nombre, apellido, edad, testimonio)
+
     formulario.reset()
 
     const divCliente = document.getElementById(`clientes`)
@@ -38,11 +65,7 @@ formulario.addEventListener(`submit`, (e)=>{
         <p><i>"${cliente.testimonio}"</i></p>
     </div>
     `
-    console.log(array)
-
 })
 })
 
-
-
-
+console.log(array)
